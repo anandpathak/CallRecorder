6ab425deb28d9a09182d7954ff020ef44f6e85a2
anand <anand.pathak@innoraft.com>
2015-11-21 02:06:15 +0530

    created class for recording and saving files

---
+++ app/src/main/java/com/example/callrecorder/SaveFile.java
@@ -0,0 +1,57 @@
+package com.example.callrecorder;
+
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by DELLANAND on 21/11/2015.
+ */
+public class SaveFile {
+
+    static MediaRecorder recorder;
+    File audiofile;
+    String audio_format;
+    public String Audio_Type;
+    int audioSource;
+    public void startRecording(){
+        Log.d("recording Started" , "Yo I have been called" );
+        String out = new SimpleDateFormat("dd-MM-yyyy hh-mm-ss").format(new Date());
+        File sampleDir = new File(Environment.getExternalStorageDirectory(), "/TestRecordingDasa1");
+        if (!sampleDir.exists()) {
+            sampleDir.mkdirs();
+        }
+        String file_name = "Record";
+        try {
+            audiofile = File.createTempFile(file_name, ".amr", sampleDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("save exception", "error" + e);
+        }
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        recorder = new MediaRecorder();
+//                          recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
+
+        recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(audiofile.getAbsolutePath());
+        try {
+            recorder.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        recorder.start();
+    }
+    public void stopRecording(){
+       recorder.stop();
+    }
+}
