f1c30929fc4998c75716dfda8d35b787c8962406
anand <anand.pathak@innoraft.com>
2015-11-21 02:05:42 +0530

    record on both incoming and outgoing

--- app/src/main/java/com/example/callrecorder/TService.java
+++ app/src/main/java/com/example/callrecorder/TService.java
@@ -42,9 +42,7 @@ public class TService extends Service {
     private static final String ACTION_IN = "android.intent.action.PHONE_STATE";
     private static final String ACTION_OUT = "android.intent.action.NEW_OUTGOING_CALL";
     private CallBr br_call;
-
-
-
+//    public SaveFile saverecordings= new SaveFile();
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -64,7 +62,7 @@ public class TService extends Service {
         // final String terminate =(String)
         // intent.getExtras().get("terminate");//
         // intent.getStringExtra("terminate");
-         Log.d("TAG", "service started");
+        Log.d("TAG", "service started");
         //
         // TelephonyManager telephony = (TelephonyManager)
         // getSystemService(Context.TELEPHONY_SERVICE); // TelephonyManager
@@ -92,7 +90,8 @@ public class TService extends Service {
         String state;
         String inCall, outCall;
         public boolean wasRinging = false;
-        public boolean setUpRecording= false;
+        public boolean setUpRecording = false;
+
 
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -102,16 +101,17 @@ public class TService extends Service {
 //                    Toast.makeText(context, "state changed : " + state, Toast.LENGTH_LONG).show();
                     if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
                         inCall = bundle.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
-                        wasRinging = true; setUpRecording=true;
+                        wasRinging = true;
+                        setUpRecording = true;
                         Toast.makeText(context, "INcoming : " + inCall, Toast.LENGTH_LONG).show();
                     } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
 
                         if (wasRinging == true) {
 
                             Toast.makeText(context, "ANSWERED", Toast.LENGTH_LONG).show();
-                            if(setUpRecording) {
+                            if (setUpRecording) {
 
-//                                String out = new SimpleDateFormat("dd-MM-yyyy hh-mm-ss").format(new Date());
+                                String out = new SimpleDateFormat("dd-MM-yyyy hh-mm-ss").format(new Date());
                                 File sampleDir = new File(Environment.getExternalStorageDirectory(), "/TestRecordingDasa1");
                                 if (!sampleDir.exists()) {
                                     sampleDir.mkdirs();
@@ -122,7 +122,7 @@ public class TService extends Service {
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                     Log.d("save exception", "error" + e);
-                                    Log.e("xxxxxx","xxxxxx");
+                                    Log.e("xxxxxx", "xxxxxx");
                                 }
                                 String path = Environment.getExternalStorageDirectory().getAbsolutePath();
 
@@ -140,19 +140,23 @@ public class TService extends Service {
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                 }
-                                recorder.start();
+                               recorder.start();
+//                                saverecordings.startRecording();
                                 recordstarted = true;
-                                setUpRecording=false;
+                                setUpRecording = false;
                             }
                         }
-//
+
                     } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
                         wasRinging = false;
                         Toast.makeText(context, "REJECT || DISCO", Toast.LENGTH_LONG).show();
                         if (recordstarted) {
                             recorder.stop();
+//                            saverecordings.stopRecording();
+                            //SaveFile.recorder.stop();
                             recordstarted = false;
                         }
+
                     }
 
                 }
@@ -160,9 +164,42 @@ public class TService extends Service {
                 if ((bundle = intent.getExtras()) != null) {
                     outCall = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
                     Toast.makeText(context, "OUT : " + outCall, Toast.LENGTH_LONG).show();
+                    //                   saverecordings.startRecording();
+                    File sampleDir = new File(Environment.getExternalStorageDirectory(), "/TestRecordingDasa1");
+                    if (!sampleDir.exists()) {
+                        sampleDir.mkdirs();
+                    }
+                    String file_name = "Record";
+                    try {
+                        audiofile = File.createTempFile(file_name, ".amr", sampleDir);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.d("save exception", "error" + e);
+                        Log.e("xxxxxx", "xxxxxx");
+                    }
+                    String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+                    recorder = new MediaRecorder();
+//                          recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
+
+                    recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
+                    recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
+                    recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                    recorder.setOutputFile(audiofile.getAbsolutePath());
+                    try {
+                        recorder.prepare();
+                    } catch (IllegalStateException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    recorder.start();
+                    recordstarted = true;
+                    setUpRecording = false;
+
                 }
             }
         }
-    }
 
+    }
 }
