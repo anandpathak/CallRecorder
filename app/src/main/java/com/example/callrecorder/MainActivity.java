12fcfc2e7e28aa0cda6b6c5f5ac8456eb133ebf8
anand <anand.pathak@innoraft.com>
2015-11-20 20:42:47 +0530

    added record call in background
    records call in background but service fails with some exception when
    answering call and service isn't working when application closed

--- app/src/main/java/com/example/callrecorder/MainActivity.java
+++ app/src/main/java/com/example/callrecorder/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.callrecorder;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,6 +15,10 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 0;
+    private DevicePolicyManager mDPM;
+    private ComponentName mAdminName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+/*        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +34,27 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+*/
+        try {
+            // Initiate DevicePolicyManager.
+            mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+            mAdminName = new ComponentName(this, DeviceAdminDemo.class);
+
+            if (!mDPM.isAdminActive(mAdminName)) {
+                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mAdminName);
+                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Click on Activate button to secure your application.");
+                startActivityForResult(intent, REQUEST_CODE);
+            } else {
+                // mDPM.lockNow();
+                // Intent intent = new Intent(MainActivity.this,
+                // TrackDeviceService.class);
+                // startService(intent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -48,5 +77,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (REQUEST_CODE == requestCode) {
+            Intent intent = new Intent(MainActivity.this, TService.class);
+            startService(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/callrecorder/TService.java
@@ -0,0 +1,159 @@
+package com.example.callrecorder;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+
+/**
+ * Created by DELLANAND on 20/11/2015.
+ */
+public class TService extends Service {
+    MediaRecorder recorder;
+    File audiofile;
+    String name, phonenumber;
+    String audio_format;
+    public String Audio_Type;
+    int audioSource;
+    Context context;
+    private Handler handler;
+    Timer timer;
+    Boolean offHook = false, ringing = false;
+    Toast toast;
+    Boolean isOffHook = false;
+    private boolean recordstarted = false;
+
+    private static final String ACTION_IN = "android.intent.action.PHONE_STATE";
+    private static final String ACTION_OUT = "android.intent.action.NEW_OUTGOING_CALL";
+    private CallBr br_call;
+
+
+
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d("service", "destroy");
+
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // final String terminate =(String)
+        // intent.getExtras().get("terminate");//
+        // intent.getStringExtra("terminate");
+         Log.d("TAG", "service started");
+        //
+        // TelephonyManager telephony = (TelephonyManager)
+        // getSystemService(Context.TELEPHONY_SERVICE); // TelephonyManager
+        // // object
+        // CustomPhoneStateListener customPhoneListener = new
+        // CustomPhoneStateListener();
+        // telephony.listen(customPhoneListener,
+        // PhoneStateListener.LISTEN_CALL_STATE);
+        // context = getApplicationContext();
+
+        final IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_OUT);
+        filter.addAction(ACTION_IN);
+        this.br_call = new CallBr();
+        this.registerReceiver(this.br_call, filter);
+
+        // if(terminate != null) {
+        // stopSelf();
+        // }
+        return START_NOT_STICKY;
+    }
+
+    public class CallBr extends BroadcastReceiver {
+        Bundle bundle;
+        String state;
+        String inCall, outCall;
+        public boolean wasRinging = false;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_IN)) {
+                if ((bundle = intent.getExtras()) != null) {
+                    state = bundle.getString(TelephonyManager.EXTRA_STATE);
+                    if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                        inCall = bundle.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                        wasRinging = true;
+                        Toast.makeText(context, "INcoming : " + inCall, Toast.LENGTH_LONG).show();
+                    } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                        if (wasRinging == true) {
+
+                            Toast.makeText(context, "ANSWERED", Toast.LENGTH_LONG).show();
+
+                            String out = new SimpleDateFormat("dd-MM-yyyy hh-mm-ss").format(new Date());
+                            File sampleDir = new File(Environment.getExternalStorageDirectory(), "/TestRecordingDasa1");
+                            if (!sampleDir.exists()) {
+                                sampleDir.mkdirs();
+                            }
+                            String file_name = "Record";
+                            try {
+                                audiofile = File.createTempFile(file_name, ".amr", sampleDir);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                Log.d("save exception", "error"+e);
+                            }
+                            String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+                            recorder = new MediaRecorder();
+//                          recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
+
+                            recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
+                            recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
+                            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                            recorder.setOutputFile(audiofile.getAbsolutePath());
+                            try {
+                                recorder.prepare();
+                            } catch (IllegalStateException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            recorder.start();
+                            recordstarted = true;
+                        }
+                    } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                        wasRinging = false;
+                        Toast.makeText(context, "REJECT || DISCO", Toast.LENGTH_LONG).show();
+                        if (recordstarted) {
+                            recorder.stop();
+                            recordstarted = false;
+                        }
+                    }
+                }
+            } else if (intent.getAction().equals(ACTION_OUT)) {
+                if ((bundle = intent.getExtras()) != null) {
+                    outCall = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+                    Toast.makeText(context, "OUT : " + outCall, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+}
