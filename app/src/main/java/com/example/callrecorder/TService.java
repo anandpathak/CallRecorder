06a43e9eaa6c47d82484f9658fb429408572b439
anand <anand.pathak@innoraft.com>
2015-12-12 00:22:19 +0530

    added feature to disable recording

--- app/src/main/java/com/example/callrecorder/TService.java
+++ app/src/main/java/com/example/callrecorder/TService.java
@@ -54,73 +54,74 @@ public class TService extends BroadcastReceiver  {
         public void onReceive(Context context, Intent intent) {
             SharedPreferences settings =context.getSharedPreferences("AUDIO_SOURCE", 0);
             SharedPreferences.Editor editor = settings.edit();
-            if (intent.getAction().equals(ACTION_IN)) {
-                if ((bundle = intent.getExtras()) != null) {
-                    state = bundle.getString(TelephonyManager.EXTRA_STATE);
+            Boolean SwitchState = settings.getBoolean("SWITCH",true);
+            if(SwitchState) {
+                if (intent.getAction().equals(ACTION_IN)) {
+                    if ((bundle = intent.getExtras()) != null) {
+                        state = bundle.getString(TelephonyManager.EXTRA_STATE);
 //                    Toast.makeText(context, "state changed : " + state, Toast.LENGTH_LONG).show();
-                        Log.d("State " ,state);
-                    if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-                        String inCall = bundle.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
-                        editor.putString("inCall",inCall);
-                        editor.commit();
-                        wasRinging = true;
-                        setUpRecording = true;
-                        //Toast.makeText(context, "INcoming : " + inCall, Toast.LENGTH_LONG).show();
-                        Log.d("Incoming Call" ," from : "+ inCall + "wasRinging : "+wasRinging);
-                    } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
-                        String inCall= settings.getString("inCall","");
-                        Log.d("message" , " Oh ! he picked the call" + inCall);
-                        if (wasRinging == true) {
+                        Log.d("State ", state);
+                        if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                            String inCall = bundle.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                            editor.putString("inCall", inCall);
+                            editor.commit();
+                            wasRinging = true;
+                            setUpRecording = true;
+                            //Toast.makeText(context, "INcoming : " + inCall, Toast.LENGTH_LONG).show();
+                            Log.d("Incoming Call", " from : " + inCall + "wasRinging : " + wasRinging);
+                        } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                            String inCall = settings.getString("inCall", "");
+                            Log.d("message", " Oh ! he picked the call" + inCall);
+                            if (wasRinging == true) {
 
-                            Toast.makeText(context, "ANSWERED", Toast.LENGTH_LONG).show();
-                            Log.d("Answered the call", "from :" + inCall);
-                            if (setUpRecording) {
-                                recordstarted = true;
-                                setUpRecording = false;
-                    //            SaveRecording.startRecording();
-                                try {
-                                    String method = settings.getString("AUDIO_SOURCE", "");
-                                    Log.d("message", method + " : " + inCall );
-                                    SaveRecording.startRecording(method,inCall,"-In");
-                                }
-                                catch (Exception e){
-                                    Log.d("Recording Exception1 : ",e.toString());
-                                }
+                                Toast.makeText(context, "ANSWERED", Toast.LENGTH_LONG).show();
+                                Log.d("Answered the call", "from :" + inCall);
+                                if (setUpRecording) {
+                                    recordstarted = true;
+                                    setUpRecording = false;
+                                    //            SaveRecording.startRecording();
+                                    try {
+                                        String method = settings.getString("AUDIO_SOURCE", "");
+                                        Log.d("message", method + " : " + inCall);
+                                        SaveRecording.startRecording(method, inCall, "-In");
+                                    } catch (Exception e) {
+                                        Log.d("Recording Exception1 : ", e.toString());
+                                    }
 
+                                }
                             }
-                        }
 
-                    } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
-                        wasRinging = false;
-                        Toast.makeText(context, "REJECT || DISCO", Toast.LENGTH_LONG).show();
-                        if (recordstarted) {
+                        } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                            wasRinging = false;
+                            Toast.makeText(context, "REJECT || DISCO", Toast.LENGTH_LONG).show();
+                            if (recordstarted) {
 //                            recorder.stop();
-                                Log.d("Message" , "Stopping recording"  );
-                            SaveRecording.stopRecording();
+                                Log.d("Message", "Stopping recording");
+                                SaveRecording.stopRecording();
 //                            saverecordings.stopRecording();
-                            //SaveFile.recorder.stop();
-                            recordstarted = false;
+                                //SaveFile.recorder.stop();
+                                recordstarted = false;
+                            }
+
                         }
 
                     }
+                } else if (intent.getAction().equals(ACTION_OUT)) {
+                    if ((bundle = intent.getExtras()) != null) {
+                        outCall = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+                        Toast.makeText(context, "OUT : " + outCall, Toast.LENGTH_LONG).show();
+                        //                   saverecordings.startRecording();
+                        try {
+                            String method = settings.getString("AUDIO_SOURCE", "");
+                            Log.d("message", method);
+                            SaveRecording.startRecording(method, outCall, "-Out");
+                        } catch (Exception e) {
+                            Log.d("Recording Exception2", e.toString());
+                        }
+                        recordstarted = true;
+                        setUpRecording = false;
 
-                }
-            } else if (intent.getAction().equals(ACTION_OUT)) {
-                if ((bundle = intent.getExtras()) != null) {
-                    outCall = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-                    Toast.makeText(context, "OUT : " + outCall, Toast.LENGTH_LONG).show();
-                    //                   saverecordings.startRecording();
-                    try {
-                        String method = settings.getString("AUDIO_SOURCE", "");
-                        Log.d("message", method);
-                        SaveRecording.startRecording(method,outCall,"-Out");
                     }
-                    catch (Exception e){
-                        Log.d("Recording Exception2", e.toString());
-                    }
-                    recordstarted = true;
-                    setUpRecording = false;
-
                 }
             }
         }
